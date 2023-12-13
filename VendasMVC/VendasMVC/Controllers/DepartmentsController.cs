9c95bbc9e4214f3ac551d5a62bfde99406338517
Miguelito2926 <juniorprol@hotmail.com>
2023-12-13 08:33:54 -0300

    Criando o Projeto MVC oficialmente

---
+++ VendasMVC/VendasMVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VendasMVC.Models;
+
+namespace VendasMVC.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> departments = new List<Department>();
+            departments.Add(new Department { Id = 1, Name = "Eletronics" });
+            departments.Add(new Department { Id = 2, Name = "Fashion" });
+            departments.Add(new Department { Id = 3, Name = "Tools" });
+            return View(departments);
+        }
+    }
+}
